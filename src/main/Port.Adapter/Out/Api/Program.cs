6381bf1ecf2647cbc19b746cc1f031d0f8cf1897
Junvic <contact@junvic.me>
2023-11-10 00:48:09 +0800

    Out.Api - replace Nancy with native minimal APIs

--- src/main/Port.Adapter/Out/Api/Program.cs
+++ src/main/Port.Adapter/Out/Api/Program.cs
@@ -1,22 +1,106 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System.Net;
+using ei8.Cortex.Graph.Application;
+using ei8.Cortex.Graph.Common;
+using ei8.Cortex.Graph.Domain.Model;
+using ei8.Cortex.Graph.Port.Adapter.IO.Persistence.ArangoDB;
+using ei8.Cortex.Graph.Port.Adapter.IO.Process.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
-namespace ei8.Cortex.Graph.Port.Adapter.Out.Api
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<ISettingsService, SettingsService>();
+builder.Services.AddScoped<INeuronRepository, NeuronRepository>();
+builder.Services.AddScoped<INeuronQueryService, NeuronQueryService>();
+builder.Services.AddScoped<ITerminalRepository, TerminalRepository>();
+builder.Services.AddScoped<ITerminalQueryService, TerminalQueryService>();
+
+builder.Services.AddHttpClient();
+
+// Add swagger UI.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add background services.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder
-                        .UseStartup<Startup>()
-                        .UseUrls("http://+:80");
-                });
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+
+// Uncomment to use HTTPS.
+//app.UseHttpsRedirection();
+
+// Add endpoints here.
+app.MapGet("/cortex/graph/neurons", async ([FromServices] INeuronQueryService neuronQueryService, HttpContext context) =>
+{
+    return await neuronQueryService.GetNeurons(ParseNeuronQueryOrEmpty(context.Request.QueryString.Value));
+});
+
+app.MapGet("/cortex/graph/neurons/{neuronId}", async ([FromServices] INeuronQueryService neuronQueryService, HttpContext context, string neuronId) =>
+{
+    return await neuronQueryService.GetNeuronById(
+                        neuronId,
+                        ParseNeuronQueryOrEmpty(context.Request.QueryString.Value)
+                        );
+});
+
+app.MapGet("/cortex/graph/neurons/{centralId}/relatives", async ([FromServices] INeuronQueryService neuronQueryService, HttpContext context, string centralId) =>
+{
+    return await neuronQueryService.GetNeurons(
+                            centralId,
+                            ParseNeuronQueryOrEmpty(context.Request.QueryString.Value)
+                            );
+});
+
+app.MapGet("/cortex/graph/neurons/{centralId}/relatives/{neuronId}", async ([FromServices] INeuronQueryService neuronQueryService, HttpContext context, string centralId, string neuronId) =>
+{
+    return await neuronQueryService.GetNeuronById(
+                        neuronId,
+                        centralId,
+                        ParseNeuronQueryOrEmpty(context.Request.QueryString.Value)
+                        );
+});
+
+app.MapGet("/cortex/graph/terminals/{terminalId}", async ([FromServices] ITerminalQueryService terminalQueryService, HttpContext context, string terminalId) =>
+{
+    return JsonConvert.SerializeObject(await terminalQueryService.GetTerminalById(
+                        terminalId,
+                        ParseNeuronQueryOrEmpty(context.Request.QueryString.Value)
+                        ));
+});
+
+app.MapGet("/cortex/graph/terminals", async ([FromServices] ITerminalQueryService terminalQueryService, HttpContext context) =>
+{
+    return JsonConvert.SerializeObject(await terminalQueryService.GetTerminals(ParseNeuronQueryOrEmpty(context.Request.QueryString.Value)));
+});
+
+// Add global exception handling
+app.UseExceptionHandler(appError =>
+{
+    appError.Run(async (context) =>
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+        var exceptionContext = context.Features.Get<IExceptionHandlerFeature>();
+
+        if (exceptionContext != null)
+            await context.Response.WriteAsync(exceptionContext.Error.ToString());
+    });
+});
+
+app.Run();
+
+static NeuronQuery ParseNeuronQueryOrEmpty(string queryString) =>
+    NeuronQuery.TryParse(queryString, out NeuronQuery query) ? query : new NeuronQuery();
